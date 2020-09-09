45b8753c0edb46d438b91a180fceca23ae8f41d8
Allen Guan <tsguan11@gmail.com>
2020-09-09 21:30:11 +0800

    Change making comment isread to bot

--- xss_ctf/board/src/Addddddddddddddmin.php
+++ xss_ctf/board/src/Addddddddddddddmin.php
@@ -5,11 +5,11 @@ if($_COOKIE['USERSESSID'] !== $_ENV['ADMIN_COOKIE']) {
     require_once('db_connection.php');
 
     $stmt = $conn->prepare('select id, content, user from comment where `isread`=0 limit 1');
+    $stmt->bind_param('i', $_GET['id']);
     $check = $stmt->execute();
     if ( $check===false ) {
         die('execute() failed: ' . htmlspecialchars($stmt->error));
-    }
-    else {
+    } else {
         $stmt->bind_result($id,$content,$user);
         $stmt->fetch();
     }
@@ -17,9 +17,4 @@ if($_COOKIE['USERSESSID'] !== $_ENV['ADMIN_COOKIE']) {
 
     $tmp = $id;
     echo $id.": ".$content;
-
-    $stmt2 = $conn->prepare('UPDATE `comment` SET `isread` = 1  WHERE `id` = ?');
-    $stmt2->bind_param('i', $tmp);
-    $stmt2->execute();
-    $stmt2->close();
 }
--- xss_ctf/bot/src/bot.py
+++ xss_ctf/bot/src/bot.py
@@ -1,11 +1,13 @@
 import time, requests, os
 from selenium import webdriver
+import mysql.connector
 
 host = os.environ.get('WEB_HOST', default='45.76.106.93')
 port = os.environ.get('WEB_PORT', default='10000')
 route = os.environ.get('WEB_URL', default='Addddddddddddddmin.php')
 
-url = f"http://{host}:{port}/{route}"
+base_url = f"http://{host}:{port}"
+url = f"{base_url}/{route}"
 print(url, flush=True)
 
 chrome_options = webdriver.ChromeOptions()
@@ -18,17 +20,38 @@ admin_cookie = {
     'value': os.environ['ADMIN_COOKIE'],
 }
 
-while True:
+def visit_page(url):
     browser = webdriver.Chrome(chrome_options=chrome_options)
     try:
-        browser.get(url)
+        browser.get(base_url)
         browser.add_cookie(admin_cookie)
         browser.get(url)
-        time.sleep(5)
+        time.sleep(2)
         print(f'Access {url}', flush=True)
     except Exception as e:
         print(e, flush=True)
-    browser.quit()
+    browser.close()
 
-    time.sleep(5)
+mysql_config = {
+    'user': 'root',
+    'password': os.environ['MYSQL_ROOT_PASSWORD'],
+    'host': os.environ['MYSQL_HOST'],
+    'database': os.environ['MYSQL_DATABASE'],
+}
 
+def mysql_execute(*opt):
+    cnx = mysql.connector.connect(**mysql_config)
+    cursor = cnx.cursor()
+    cursor.execute(*opt)
+    res = list(cursor)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return res
+
+while True:
+    comments = mysql_execute('select id from comment where `isread` = 0')
+    for [idx] in comments:
+        visit_page(f"{url}?id={idx}")
+        mysql_execute('UPDATE `comment` SET `isread` = 1  WHERE `id` = %s', [idx])
+    time.sleep(10)
